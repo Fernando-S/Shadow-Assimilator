e078fd4674527f6baa408faf0d3b9bcc0ea717ad
PedroASNegrao <45979011+PedroASNegrao@users.noreply.github.com>
2019-06-04 23:21:26 -0300

    Add files via upload

--- src/PenguinBody.cpp
+++ src/PenguinBody.cpp
@@ -27,13 +27,13 @@ PenguinBody::PenguinBody(GameObject& associated) : Component(associated)/*, pcan
 	sprite_idleL = new Sprite(associated, "./assets/img/sprite_idle.png", 12, 0.1);
 	SetSprite("./assets/img/sprite_idle.png", 12, 0.1);
 	
-	associated.AddComponent(sprite);
-	//associated.AddComponent(sprite_idle);
-	//associated.AddComponent(sprite_running);
+	//associated.AddComponent(sprite);
+	associated.AddComponent(sprite_idleR);
+	//associated.AddComponent(sprite_runningR);
 	//associated.ChangeComponent(sprite, sprite_idle);
 
-	associated.AddComponent(new Collider(associated));
-	associated.angleDeg = angle * 180 / PI;
+	//associated.AddComponent(new Collider(associated));
+	//associated.angleDeg = angle * 180 / PI;
 }
 
 PenguinBody::~PenguinBody() {
@@ -63,55 +63,6 @@ void PenguinBody::Update(float dt) {
 		//pcannon.lock()->RequestDelete();
 		Camera::Unfollow();				// Camera para de segui-los
 		
-										/*
-		// Toca o som e mostra a explosao
-		auto explosionGO = new GameObject();
-		auto explosionSound = new Sound(*explosionGO, "./assets/audio/boom.wav");
-		explosionGO->AddComponent(new Sprite(*explosionGO, "./assets/img/penguindeath.png", 5, 0.1, 1.5));
-		explosionGO->AddComponent(explosionSound);
-		explosionSound->Play();
-		explosionGO->box.PlaceCenter(associated.box.Center());
-		Game::GetInstance().GetCurrentState().AddObject(explosionGO);
-		*/
-
-
-		/*
-		// Tentativa do Negrao de fazer animacao funcionar
-		if ((Getspeed2 == true) && (Setrun == true)) {
-			Setrun = false;
-			associated.RemoveComponent(sprite);
-			auto CorridaGO = new GameObject();
-			auto explosionSound = new Sound(*CorridaGO, "./assets/audio/boom.wav");
-			CorridaGO->AddComponent(new Sprite(*CorridaGO, "./assets/img/sprite_corrida2.png", 12, 0.1, 1.5));
-			//system("PAUSE");
-			CorridaGO->AddComponent(explosionSound);
-			explosionSound->Play();
-			CorridaGO->box.PlaceCenter(associated.box.Center());
-			Game::GetInstance().GetCurrentState().AddObject(CorridaGO);
-
-			std::cout << "\n\nCORRIDA\n\n";
-			associated.AddComponent(sprite);
-		}
-
-		if ((Getspeed2 == false) && (Setidle == true)) {
-			std::cout << "Setidle: " << Setidle << endl;
-			std::cout << "Getspeed2: " << Getspeed2 << endl;
-			system("PAUSE");
-			Setidle = false;
-			associated.RemoveComponent(sprite);
-			auto IdleGO = new GameObject();
-			auto explosionSound = new Sound(*IdleGO, "./assets/audio/boom.wav");
-			IdleGO->AddComponent(new Sprite(*IdleGO, "./assets/img/sprite_idle.png", 12, 0.1, 1.5));
-			//system("PAUSE");
-			IdleGO->AddComponent(explosionSound);
-			explosionSound->Play();
-			IdleGO->box.PlaceCenter(associated.box.Center());
-			Game::GetInstance().GetCurrentState().AddObject(IdleGO);
-
-			std::cout << "\n\nCORRIDA\n\n";
-			associated.AddComponent(sprite);
-		}
-		*/
 
 
 
@@ -167,14 +118,20 @@ void PenguinBody::Update(float dt) {
 		}
 		else if (inputManager.IsKeyDown(D_KEY)) {
 			speed = { 1, 0 };
-
+			
 			if (Getspeed2 == false) {
 				oppositeSpeed = linearSpeed;
 				std::cout << "\n\nPassarvalor2" << endl << endl;
 				Getspeed2 = true;
 
-			//	Setrun = true;
-			//	SwitchSetidle = 0;
+				Stop = 0;
+				Setidle = false;
+				Setrun = true;
+				
+			}
+
+			if (Run < 10) {
+				Run++;
 			}
 
 			if (linearSpeed < PLAYER_SPEED + 50) {
@@ -187,16 +144,22 @@ void PenguinBody::Update(float dt) {
 					linearSpeed += accelSpeedGain;
 
 			}
-			std::cout << "linearSpeed2: " << linearSpeed << endl;
-			std::cout << "oppositeSpeed2: " << oppositeSpeed << endl;
-			//cout << linearSpeed << endl;
 
-			// Controle de flags de animacao
-			setaNovoSprite = true;
-			startedMovingR = true;
-			startedMovingL = false;
-			idleR = false;
-			idleL = false;
+			//cout << "Run: " << Run << endl;
+
+			//std::cout << "linearSpeed2: " << linearSpeed << endl;
+			//std::cout << "oppositeSpeed2: " << oppositeSpeed << endl;
+			//cout << linearSpeed << endl;
+			
+			/*
+			//COISAS DO FERNANDO
+				// Controle de flags de animacao
+				setaNovoSprite = true;
+				startedMovingR = true;
+				startedMovingL = false;
+				idleR = false;
+				idleL = false;
+			*/
 		}
 
 		double atrictSpeedLoss = PENGUIN_ATRICT * dt;
@@ -204,42 +167,28 @@ void PenguinBody::Update(float dt) {
 		if (!(inputManager.IsKeyDown(A_KEY)))
 			Getspeed1 = false;
 
-		if (!(inputManager.IsKeyDown(D_KEY)))
-			Getspeed2 = false;
-
-		/*
-		// Tambem eh tentativa do Negrao de fazer a animacao funcion/ar
 		if (!(inputManager.IsKeyDown(D_KEY))) {
-			//a avariável SwitchSetidle serve para que Setidle seja setado apenas uma vez
-			if ((Setidle == false) && (SwitchSetidle == false)) {
-				Setidle = true;
-				SwitchSetidle = true;
-			}
-			else 
-				Setidle = false;
-
 			Getspeed2 = false;
 		}
-		*/
 
 		if ((!(inputManager.IsKeyDown(A_KEY))) && (!(inputManager.IsKeyDown(D_KEY))) && (!(inputManager.IsKeyDown(W_KEY))) && (!(inputManager.IsKeyDown(S_KEY)))) {
 
-			if (linearSpeed > 40)
+			if (linearSpeed > 40) {
 				linearSpeed -= accelSpeedGain;
-			else if (linearSpeed < -40)
+			}
+			if (linearSpeed < -40) {
 				linearSpeed += accelSpeedGain;
-			else if (linearSpeed < 40) {
-				if (linearSpeed != 0)
-					linearSpeed--;
 			}
-			else if (linearSpeed > -40) {
-				if (linearSpeed != 0)
-					linearSpeed--;
+			if ((linearSpeed < 40) && (linearSpeed > -40)) {
+				linearSpeed = 0;
 			}
-			//std::cout << "linearSpeed: " << linearSpeed << endl;
+			
 
-			//startedMoving = false;
-			//idle = true;
+			//cout << "linearSpeed: " << linearSpeed << endl;
+			//cout << "Stop: " << Stop << endl;
+
+			//startedMovingR = false;
+			//idleR = true;
 			//setaNovoSprite = true;
 		}
 
@@ -257,57 +206,85 @@ void PenguinBody::Update(float dt) {
 		else {
 			linearSpeed = 0;
 
+			if (Stop < 10) {
+				Stop++;
+			}
+			Run = 0;
+			Setidle = true;
+			Setrun = false;
 
-			startedMovingR = false;
-			idleR = true;
-			setaNovoSprite = true;
+			//startedMovingR = false;
+			//idleR = true;
+			//setaNovoSprite = true;
 		}
 
-	}
-
-
-
-
-	// todo - pensar em como trocar de parado para andando e de andando para parado
-	if (setaNovoSprite) {
-	//	associated.RemoveComponent(sprite);
-	//	if (startedMoving && !alreadyMoving) {
-	//		SetSprite("./assets/img/sprite_corrida2.png", 12, 0.1);
-	//		startedMoving = false;
-	//		alreadyMoving = true;
-	//6	}
-	//	else if (idle) {
-	//		SetSprite("./assets/img/sprite_idle.png", 12, 0.1);
-	//		idle = false;
-	//	}
-		//associated.AddComponent(sprite);
-		if (startedMovingR) {
-			associated.ChangeComponent(sprite, sprite_runningR);
-			//associated.ChangeComponent(sprite_idleR, sprite_runningR);
-			
-			//sprite->Open("./assets/img/sprite_corrida2.png");
-			//sprite->SetFrameCount(12);
-			//sprite->SetFrameTime(0.1);
-			//sprite = new Sprite(associated, "./assets/img/sprite_corrida2.png", 12, 0.1);
+		if ((Stop == 1) || (Run == 1)) {
+			//cout << "\n\nSetidle: " << Setidle << "\nSetrun: " << Setrun << "\nStop: " << Stop << "\nlinearspeed: " << linearSpeed << "\n\n";
 		}
-		else if (idleR) {
-			associated.ChangeComponent(sprite_runningR, sprite_idleR);
-			//associated.ChangeComponent(sprite_runningR, sprite);
+		/*
+		if (inicio == true) {
+			associated.ChangeComponent(sprite, sprite_idleR);
+			Stop = 2;
+			inicio = false;
+			cout << "\n\nTROCA, PARA > CORRE-INICIO\n\n";
 		}
-		/*else if (startedMovingL) {
-			//associated.ChangeComponent(sprite, sprite_runningR);
-			associated.ChangeComponent(sprite_idleR, sprite_runningR);
-		}
-		else if (idleR)
-			associated.ChangeComponent(sprite_runningR, sprite_idleR);
 		*/
 
-		setaNovoSprite = false;
+		if ((Setidle == true) && (Setrun == false) && (Stop == 1)) {	//para
+			associated.ChangeComponent(sprite_runningR, sprite_idleR);
+			cout << "\nTROCA, CORRE > PARA\n\n";
+		}
+		if ((Setidle == false) && (Setrun == true) && (Run == 1)) {		//corre
+			associated.ChangeComponent(sprite_idleR, sprite_runningR);
+			cout << "\nTROCA, PARA > CORRE\n\n";
+		}
+		
 	}
 
 	
-}
 
+	//COISAS DO FERNANDO
+			// todo - pensar em como trocar de parado para andando e de andando para parado
+	//		if (setaNovoSprite) {
+			//	associated.RemoveComponent(sprite);
+			//	if (startedMoving && !alreadyMoving) {
+			//		SetSprite("./assets/img/sprite_corrida2.png", 12, 0.1);
+			//		startedMoving = false;
+			//		alreadyMoving = true;
+			//6	}
+			//	else if (idle) {
+			//		SetSprite("./assets/img/sprite_idle.png", 12, 0.1);
+			//		idle = false;
+			//	}
+				//associated.AddComponent(sprite);
+	//			if (startedMovingR) {
+	//				associated.ChangeComponent(sprite, sprite_runningR);
+					//associated.ChangeComponent(sprite_idleR, sprite_runningR);
+			
+					//sprite->Open("./assets/img/sprite_corrida2.png");
+					//sprite->SetFrameCount(12);
+					//sprite->SetFrameTime(0.1);
+					//sprite = new Sprite(associated, "./assets/img/sprite_corrida2.png", 12, 0.1);
+	//				cout << "move" << endl;
+	//		}
+	//			else if (idleR) {
+	//				associated.ChangeComponent(sprite_runningR, sprite_idleR);
+					//associated.ChangeComponent(sprite_runningR, sprite);
+	//				cout << "para" << endl;
+	//			}
+				/*else if (startedMovingL) {
+					//associated.ChangeComponent(sprite, sprite_runningR);
+					associated.ChangeComponent(sprite_idleR, sprite_runningR);
+				}
+				else if (idleR)
+					associated.ChangeComponent(sprite_runningR, sprite_idleR);
+				*/
+
+	//			setaNovoSprite = false;
+	//		}
+
+	
+}
 
 void PenguinBody::Render() {
 
