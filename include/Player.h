898c9f734b3fc6e7ddeb6599f9606b3d0c9682d2
Fernando-S <31493374+Fernando-S@users.noreply.github.com>
2019-07-06 11:24:01 -0300

    Add files via upload

--- include/Player.h
+++ include/Player.h
@@ -44,13 +44,13 @@ public:
 
 private:
 	//std::weak_ptr<GameObject> pcannon;
-	Timer WallJumpTimer;
+	Timer WallJumpTimer, DeathTimer;
 	Timer ShootCooldownTimer, DashCooldownTimer;
 	Vec2 speedH, speedV, speedD;
 	float linearSpeed = 0, verticalSpeed = 0, diagonalSpeed = 0, angle, gravity = 1.0, oppositeAccel, oppositeSpeed;
 	bool Getspeed1 = false, Getspeed2 = false, Setrun = false, Setidle = true, WallgrabL = false, WallgrabR = false,
 		 tchfloor, airbone, doubleJump = false, tchCeiling = false, facingR = true, facingL = false, WallJump = false,
-		 SetJump = false, runningSound = false, wallSlideSound = false;
+		 SetJump = false, runningSound = false, wallSlideSound = false, dead = false;
 	int hp, Stop = 0, Run = 0, Jump = 0, Fall = 0, wallAUX = 0, wallX = 0, Ground = 0, DJ = 0, BuzzL = 0;
 	int contadorW1 = 0, contadorW2 = 0;
 
