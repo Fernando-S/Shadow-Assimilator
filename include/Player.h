6b92f5918243b24e9c2834995962a6ae53306c71
Fernando-S <31493374+Fernando-S@users.noreply.github.com>
2019-07-07 17:00:38 -0300

    Add files via upload

--- include/Player.h
+++ include/Player.h
@@ -8,11 +8,14 @@
 #include "Vec2.h"
 #include "GameObject.h"
 #include "Sprite.h"
-#include "Collider.h"
-#include "PenguinCannon.h"
-#include "InputManager.h"
 #include "Music.h"
 #include "Sound.h"
+#include "Collider.h"
+#include "Laser.h"
+#include "TileMap.h"
+#include "Camera.h"
+//#include "PenguinCannon.h"
+#include "InputManager.h"
 
 
 #define PLAYER_MAX_LINEAR_SPEED 500		// em pixels/s
@@ -21,7 +24,54 @@
 #define PLAYER_ANGULAR_SPEED PI			// em rads/s
 #define PLAYER_INITIAL_HP 22			// em hit points
 #define ONETILESQUARE 64				// tamanho de 1 tile
-#define PLAYER_BULLET_DAMAGE 2			// em hit points
+#define PLAYER_LASER_DAMAGE 2			// em hit points
+
+/*
+class Player : public Component {
+public:
+	Player(GameObject& associated);
+	~Player();
+	void Start() override;
+	void Update(float dt) override;
+	void Render() override;
+	bool Is(std::string type) override;
+	void NotifyCollision(GameObject& other) override;
+	Vec2 GetCenter();			// Funcao para poder pegar a posicao do player
+	void Shoot(Vec2 target);
+	int GetHP();
+
+	static Player* player;
+	Sprite* sprite;
+	bool damaged = false, isAtacking = false, ultrapassou = false;
+	Sound *runSFX, *jumpSFX, *DjumpSFX, *landSFX, *LightAttackSFX, *HeavyAttackSFX, *playerSFX;
+
+private:
+	//std::weak_ptr<GameObject> pcannon;
+	Timer WallJumpTimer, DeathTimer;
+	Timer ShootCooldownTimer, DashCooldownTimer;
+	Vec2 speedH, speedV, speedD;
+	float linearSpeed = 0, verticalSpeed = 0, diagonalSpeed = 0, angle, gravity = 1.0, oppositeAccel, oppositeSpeed;
+	bool Getspeed1 = false, Getspeed2 = false, Setrun = false, Setidle = true, WallgrabL = false, WallgrabR = false,
+		 tchfloor, airbone, doubleJump = false, tchCeiling = false, facingR = true, facingL = false, WallJump = false,
+		 SetJump = false, runningSound = false, wallSlideSound = false, dead = false;
+	int hp, Stop = 0, Run = 0, Jump = 0, Fall = 0, wallAUX = 0, wallX = 0, Ground = 0, DJ = 0, BuzzL = 0;
+	int contadorW1 = 0, contadorW2 = 0;
+
+};
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Player : public Component {
 public:
@@ -55,7 +105,6 @@ private:
 	int contadorW1 = 0, contadorW2 = 0;
 
 };
-#endif	//PLAYER_H
 
 
 
@@ -69,59 +118,9 @@ private:
 
 
 
-/*
-#pragma once
-
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "Component.h"
-#include "Vec2.h"
-#include "GameObject.h"
-#include "Sprite.h"
-#include "Collider.h"
-#include "PenguinCannon.h"
-#include "InputManager.h"
-#include "Music.h"
 
 
-#define PLAYER_MAX_LINEAR_SPEED 500		// em pixels/s
-#define PLAYER_ACCELERATION 1000		// em pixels/s²
-#define PLAYER_ATRICT 150				// em pixels/s²
-#define PLAYER_ANGULAR_SPEED PI			// em rads/s
-#define PLAYER_INITIAL_HP 22			// em hit points
-#define ONETILESQUARE 64				// tamanho de 1 tile
 
-class Player : public Component {
-public:
-	Player(GameObject& associated);
-	~Player();
-	void Start() override;
-	void Update(float dt) override;
-	void Render() override;
-	bool Is(std::string type) override;
-	void NotifyCollision(GameObject& other) override;
-	Vec2 GetCenter();			// Funcao para poder pegar a posicao do player
-	void Shoot(Vec2 target);
-	int GetHP();
 
-	static Player* player;
-	//float secondsToSelfDestruction = 1.5;
-	Sprite* sprite;
-	bool damaged = false;
-
-private:
-	//std::weak_ptr<GameObject> pcannon;
-	Timer WallJumpTimer;
-	Timer ShootCooldownTimer;
-	Vec2 speedH, speedV, speedD;
-	float linearSpeed = 0, verticalSpeed = 0, diagonalSpeed = 0, angle, gravity = 1.0, oppositeAccel, oppositeSpeed;
-	bool Getspeed1 = false, Getspeed2 = false, Setrun = false, Setidle = true, WallgrabL = false, WallgrabR = false,
-		tchfloor, airbone, doubleJump = false, tchCeiling = false, facingR = true, facingL = false, WallJump = false,
-		SetJump = false;
-	int hp, Stop = 0, Run = 0, Jump = 0, Fall = 0, wallAUX = 0, wallX = 0, Ground = 0;
-
-};
 
 #endif	//PLAYER_H
-*/
