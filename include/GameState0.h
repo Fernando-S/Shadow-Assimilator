6eb57b5814031594ddfdd09a80ddbae17f348c02
Fernando-S <31493374+Fernando-S@users.noreply.github.com>
2019-07-11 17:31:41 -0300

    Add files via upload

--- include/GameState0.h
+++ include/GameState0.h
@@ -49,6 +49,8 @@ private:
 	GameObject* finalBossGo;
 	Player* player;
 
+	Timer playerHitTimer, robotHitTimer;
+
 	bool dois = false, tres = false, quatro = false, cinco = false, meia = false, sete = false, oito = false, mostrando = false;
 	bool cima1 = false, cima2 = false, baixo1 = false, baixo2 = false, esquerda1 = false, esquerda2 = false, direita1 = false, direita2 = false;
 	
