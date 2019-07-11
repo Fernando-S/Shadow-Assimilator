e3f1f38ed8e4d4181d34d436c410e7dc44a07662
PedroASNegrao <45979011+PedroASNegrao@users.noreply.github.com>
2019-07-11 01:09:14 -0300

    Add files via upload

--- include/Player.h
+++ include/Player.h
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #ifndef PLAYER_H
@@ -13,6 +14,7 @@
 #include "Laser.h"
 #include "TileMap.h"
 #include "Camera.h"
+//#include "PenguinCannon.h"
 #include "InputManager.h"
 
 
@@ -23,9 +25,56 @@
 #define PLAYER_INITIAL_HP 22			// em hit points
 #define ONETILESQUARE 64				// tamanho de 1 tile
 #define PLAYER_LASER_DAMAGE 2			// em hit points
-#define LASER_SPEED 1000				// em pixels/s
-#define LASER_MAX_DAMAGE 20				// em hit points
-#define LASER_MAX_DISTANCE 1000			// em pixels
+#define LASER_SPEED 1000			// em pixels/s
+#define LASER_MAX_DAMAGE 20			// em hit points
+#define LASER_MAX_DISTANCE 1000		// em pixels
+
+
+/*
+class Player : public Component {
+public:
+	Player(GameObject& associated);
+	~Player();
+	void Start() override;
+	void Update(float dt) override;
+	void Render() override;
+	bool Is(std::string type) override;
+	void NotifyCollision(GameObject& other) override;
+	Vec2 GetCenter();			// Funcao para poder pegar a posicao do player
+	void Shoot(Vec2 target);
+	int GetHP();
+
+	static Player* player;
+	Sprite* sprite;
+	bool damaged = false, isAtacking = false, ultrapassou = false;
+	Sound *runSFX, *jumpSFX, *DjumpSFX, *landSFX, *LightAttackSFX, *HeavyAttackSFX, *playerSFX;
+
+private:
+	//std::weak_ptr<GameObject> pcannon;
+	Timer WallJumpTimer, DeathTimer;
+	Timer ShootCooldownTimer, DashCooldownTimer;
+	Vec2 speedH, speedV, speedD;
+	float linearSpeed = 0, verticalSpeed = 0, diagonalSpeed = 0, angle, gravity = 1.0, oppositeAccel, oppositeSpeed;
+	bool Getspeed1 = false, Getspeed2 = false, Setrun = false, Setidle = true, WallgrabL = false, WallgrabR = false,
+		 tchfloor, airbone, doubleJump = false, tchCeiling = false, facingR = true, facingL = false, WallJump = false,
+		 SetJump = false, runningSound = false, wallSlideSound = false, dead = false;
+	int hp, Stop = 0, Run = 0, Jump = 0, Fall = 0, wallAUX = 0, wallX = 0, Ground = 0, DJ = 0, BuzzL = 0;
+	int contadorW1 = 0, contadorW2 = 0;
+
+};
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class Player : public Component {
@@ -48,6 +97,7 @@ public:
 	Sound* playerSFX;
 
 private:
+	//std::weak_ptr<GameObject> pcannon;
 	Timer WallJumpTimer, DeathTimer, DJTimer;
 	Timer ShootCooldownTimer, DashCooldownTimer, ATK1CooldownTimer;
 	Vec2 speedH, speedV, speedD;
@@ -57,8 +107,25 @@ private:
 		SetJump = false, runningSound = false, wallSlideSound = false, deathSound = false, dead = false, notDJ = false,
 		pouso = false, foguete = false, Atk0 = false, Neon = false;
 	int hp, Stop = 0, Run = 0, Jump = 0, Fall = 0, wallAUX = 0, wallX = 0, Ground = 0, DJ = 0, BuzzL = 0, Atk1 = 0, Atk2 = 0;
-	int contadorW1 = 0, contadorW2 = 0, contadorATK = 0, froze = 0, Atk2delay = 0;
+	int contadorW1 = 0, contadorW2 = 0, contadorATK = 0, froze = 0, Atk2delay = 0, shootaux = 0;
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif	//PLAYER_H
