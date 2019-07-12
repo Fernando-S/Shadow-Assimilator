cbf72ef884beda13e25f9ae7e015d5e94187d7ab
Fernando-S <31493374+Fernando-S@users.noreply.github.com>
2019-07-12 00:16:39 -0300

    Add files via upload

--- include/GameState2.h
+++ include/GameState2.h
@@ -45,9 +45,11 @@ private:
 	GameObject* surpriseGO;
 	GameObject* playerGO;
 	GameObject* coatGuyGO;
+	GameObject* finalBossGO;
 	GameObject* chaoGO;
 	Player* player;
-	GameObject* finalBossGo;
+	CoatGuy* coatGuy;
+	FinalBoss* finalBoss;
 
 	bool dois = false, tres = false, quatro = false, cinco = false, meia = false, sete = false, oito = false, mostrando = false;
 	bool cima1 = false, cima2 = false, baixo1 = false, baixo2 = false, esquerda1 = false, esquerda2 = false, direita1 = false, direita2 = false;
